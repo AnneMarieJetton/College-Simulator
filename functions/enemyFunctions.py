89e33c39784e9328b863bca54db97b58a8ef5a61
Treyson Grange <treyson.grange@outlook.com>
2022-03-19 03:41:42 -0600

    experimental

--- functions/enemyFunctions.py
+++ functions/enemyFunctions.py
@@ -2,6 +2,6 @@ import pygame
 import random
 
 def generateEnemy(screen):
-    enemy_surf = pygame.transform.scale2x(pygame.image.load("Assets/Sprites/Enemy.png.png"))
+    enemy_surf = pygame.transform.scale2x(pygame.image.load("Assets/Sprites/Enemy.png.png")).convert_alpha()
     enemy_rect = enemy_surf.get_rect(center = (700,275))
     screen.blit(enemy_surf,enemy_rect)
