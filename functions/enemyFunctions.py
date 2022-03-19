32a085cc5685396754347c8a3faead614d47bad5
Treyson Grange <treyson.grange@outlook.com>
2022-03-19 04:45:13 -0600

    erikified

--- functions/enemyFunctions.py
+++ functions/enemyFunctions.py
@@ -2,6 +2,10 @@ import pygame
 import random
 
 def generateEnemy(screen):
-    enemy_surf = pygame.transform.scale2x(pygame.image.load("Assets/Sprites/Enemy.png.png")).convert_alpha()
+    font = pygame.font.SysFont("Calibri", 25, True, False)
+    enemy_surf = pygame.transform.scale2x(pygame.image.load("Assets/Sprites/pixil-frame-0.png")).convert_alpha()
     enemy_rect = enemy_surf.get_rect(center = (700,275))
+    enemy_speech = font.render("Talk to your ducky",False,(225,225,225))
+    enemy_speech_rect = enemy_speech.get_rect(center = (700,200))
     screen.blit(enemy_surf,enemy_rect)
+    screen.blit(enemy_speech,enemy_speech_rect)
