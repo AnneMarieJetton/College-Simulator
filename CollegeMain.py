3437e7a5add7e959f9ccb8c59ba815c64fd80e46
Emily McBride <a02291674@usu.edu>
2022-03-18 21:11:44 -0600

    enemy square exists

--- CollegeMain.py
+++ CollegeMain.py
@@ -3,6 +3,9 @@
 import pygame
 import sys
 import random
+from functions import enemyFunctions
+
+
 
 pygame.init()
 
@@ -15,13 +18,36 @@ done = False
 
 clock = pygame.time.Clock()
 fps = 25
+x = 50
+y = 440
+width = 40
+height = 60
+vel = 5
+
+level = 1
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
     screen.fill(BACKGROUND_COLOR)
+
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+    enemyFunctions.generateEnemy(screen)
     clock.tick(30)
+
     pygame.display.update()
 
 pygame.quit()
