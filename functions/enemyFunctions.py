49bc4035ab03f3f8beb3eeefd20c5b70f646a171
Emily McBride <a02291674@usu.edu>
2022-03-19 07:40:34 -0600

    more quotes

--- functions/enemyFunctions.py
+++ functions/enemyFunctions.py
@@ -4,15 +4,19 @@ import random
 def generateEnemy(screen, numAttacks):
 
     #text picking.
-    textList = ["Talk to your Ducky", "Eval is Evil"]
+    textList = ["Talk to your Ducky", "Eval is Evil!", "Scrum Meeting!", "Cheating is bad!!!"]
 
     font = pygame.font.Font('Assets/Fonts/Pixeltype.ttf', 30)
     enemy_surf = pygame.transform.scale2x(pygame.image.load("Assets/Sprites/pixil-frame-0.png")).convert_alpha()
     enemy_rect = enemy_surf.get_rect(center = (700,275))
-    if numAttacks == 1 or numAttacks == 3:
+    if numAttacks == 1:
         enemy_speech = font.render(str(textList[0]),False,(225,225,225))
-    else:
+    elif numAttacks == 2:
         enemy_speech = font.render(str(textList[1]), False, (225, 225, 225))
+    elif numAttacks == 3:
+        enemy_speech = font.render(str(textList[2]), False, (225, 225, 225))
+    else:
+        enemy_speech = font.render(str(textList[3]), False, (225, 225, 225))
 
 
     enemy_speech_rect = enemy_speech.get_rect(center = (700,200))
