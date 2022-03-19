bf4cc74d541e2dd2b962875c39d57a3a6f9a1583
Anne Jetton <annemariejetton@gmail.com>
2022-03-19 11:37:35 -0600

    energy drinks

--- functions/attackFunctions.py
+++ functions/attackFunctions.py
@@ -12,6 +12,18 @@ def attackMovement(attackList,screen,bullet):
     else:
         return []
 
+def healMovement(healList,screen,bullet):
+    if healList:
+        for attackRect in healList:
+            attackRect.x -= 10
+            #screen.fill((0,0,0))
+            screen.blit(bullet, attackRect)
+
+        attackList = [attack for attack in healList if attack.x > -100]
+        return attackList
+    else:
+        return []
+
 
 def collisions(player, attacks, playerHealth):
     if attacks:
@@ -21,4 +33,12 @@ def collisions(player, attacks, playerHealth):
                 return playerHealth
     return playerHealth
 
+def heal(player, attacks, playerHealth):
+    if attacks:
+        for attackRect in attacks:
+            if player.colliderect(attackRect):
+                if playerHealth != 50:
+                    playerHealth = playerHealth + 1
+                return playerHealth
+    return playerHealth
 
