d0eda8a373408f935e0a60fa8e6ee8ba1446ed15
Anne Jetton <annemariejetton@gmail.com>
2022-03-19 02:39:38 -0600

    multiple attacks

--- CollegeMain.py
+++ CollegeMain.py
@@ -10,86 +10,102 @@ from functions import attackFunctions
 
 pygame.init()
 
-size = (800, 600)
+gameDone = False
+while not gameDone:
+    #menu loop
 
-BACKGROUND_COLOR = (0, 0, 0)
+    
 
-screen = pygame.display.set_mode(size)
-done = False
-x = 50
-y = 275
-player_surf = pygame.image.load("Assets/Sprites/TestPlayer.png")
-player_rect = player_surf.get_rect(center = (x,y))
+    levelDone = False
+    while not levelDone:
+        numAttacks = 0
+        #level loop
 
-attack_surf = pygame.image.load("Assets/Sprites/PelletAttackTest.png")
+        size = (800, 600)
 
-clock = pygame.time.Clock()
-fps = 25
+        BACKGROUND_COLOR = (0, 0, 0)
 
-width = 40
-height = 60
-vel = 5
+        screen = pygame.display.set_mode(size)
+        attackDone = False
+        x = 50
+        y = 275
+        player_surf = pygame.image.load("Assets/Sprites/TestPlayer.png")
+        player_rect = player_surf.get_rect(center = (x,y))
 
-level = 1
-timer = pygame.USEREVENT + 1
-pygame.time.set_timer(timer,1)
-attackList = []
+        attack_surf = pygame.image.load("Assets/Sprites/PelletAttackTest.png")
 
-playerHealth = 50
+        clock = pygame.time.Clock()
+        fps = 25
 
-levelTimer = 20
+        width = 40
+        height = 60
+        vel = 5
 
-while not done:
+        level = 1
+        timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(timer,1)
+        attackList = []
 
+        playerHealth = 50
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        levelTimer = 20
 
-    keys = pygame.key.get_pressed()
-
-    if len(attackList) != 0:
-        if keys[pygame.K_LEFT]:
-            player_rect.x -= vel
-        if keys[pygame.K_RIGHT]:
-            player_rect.x += vel
-        if keys[pygame.K_UP]:
-            player_rect.y -= vel
-        if keys[pygame.K_DOWN]:
-            player_rect.y += vel
-
-    if player_rect.y < 0:
-        player_rect.y = 0
-    if player_rect.y > 575:
-        player_rect.y = 575
-    if player_rect.x > 600:
-        player_rect.x = 600
-    if player_rect.x < 0:
-        player_rect.x = 0
-
-    screen.fill(BACKGROUND_COLOR)
-
-    if event.type == timer:
-        delay = random.random()
-        if delay < .1:
-            if levelTimer != 0:
-                attackList.append(attack_surf.get_rect(center = (random.randint(900,1100),random.randint(0,600))))
-                levelTimer = levelTimer - 1
-                print(len(attackList))
-            else:
-                print(attackList[len(attackList) - 1].x)
-                if attackList[len(attackList) - 1].x <= 0:
-                    done = True
-
-    screen.blit(player_surf,player_rect)
-    enemyFunctions.generateEnemy(screen)
-    attackFunctions.attackMovement(attackList,screen,attack_surf)
+        while not attackDone:
+            #attack loop
 
 
-    pygame.display.update()
-    clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    attackDone = True
 
-    playerHealth = attackFunctions.collisions(player_rect, attackList, playerHealth)
-    if playerHealth <= 0:
-        done = True
+            keys = pygame.key.get_pressed()
+
+            if len(attackList) != 0:
+                if keys[pygame.K_LEFT]:
+                    player_rect.x -= vel
+                if keys[pygame.K_RIGHT]:
+                    player_rect.x += vel
+                if keys[pygame.K_UP]:
+                    player_rect.y -= vel
+                if keys[pygame.K_DOWN]:
+                    player_rect.y += vel
+
+            if player_rect.y < 0:
+                player_rect.y = 0
+            if player_rect.y > 575:
+                player_rect.y = 575
+            if player_rect.x > 600:
+                player_rect.x = 600
+            if player_rect.x < 0:
+                player_rect.x = 0
+
+            screen.fill(BACKGROUND_COLOR)
+
+            if event.type == timer:
+                delay = random.random()
+                if delay < .1:
+                    if levelTimer != 0:
+                        attackList.append(attack_surf.get_rect(center = (random.randint(900,1100),random.randint(0,600))))
+                        levelTimer = levelTimer - 1
+                        # print(len(attackList))
+                    else:
+                        # print(attackList[len(attackList) - 1].x)
+                        if attackList[len(attackList) - 1].x <= 0:
+                            attackDone = True
+
+            screen.blit(player_surf,player_rect)
+            enemyFunctions.generateEnemy(screen)
+            attackFunctions.attackMovement(attackList,screen,attack_surf)
+
+
+            pygame.display.update()
+            clock.tick(30)
+
+            playerHealth = attackFunctions.collisions(player_rect, attackList, playerHealth)
+            if playerHealth <= 0:
+                attackDone = True
+
+        numAttacks = numAttacks + 1
+        if numAttacks == 4:
+            levelDone = True
 pygame.quit()
